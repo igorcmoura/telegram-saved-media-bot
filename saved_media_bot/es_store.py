754facac4f59051f4119d5e7257f72adb9f16eaf
Igor Cescon de Moura <igorcmgamer@gmail.com>
2021-01-09 03:14:30 -0300

    refactor: remove unused import

--- saved_media_bot/es_store.py
+++ saved_media_bot/es_store.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import datetime
 from typing import Dict, Iterable
 from elasticsearch import Elasticsearch
 from elasticsearch.exceptions import NotFoundError
